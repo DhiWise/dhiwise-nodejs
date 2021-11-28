248613c9205693e4ba78e697a53327bc9fe0567f
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-11-28 12:53:39 +0530

    default props added and tailwind padding classes updated

--- packages/client/src/components/MenuList/index.js
+++ packages/client/src/components/MenuList/index.js
@@ -6,53 +6,59 @@ import { MenulistCss } from './menuListCss';
 export const MenuBox = ({
   link,
   menuClass,
-  onClick,
   icon,
   textCss,
   title,
   iconClass,
-  tooltip = false,
+  tooltip,
+  onClick,
 }) => (
   <div
     data-tip
     data-for={`tooltip${tooltip}`}
-    className={`${MenulistCss.menuItem} ${!link && MenulistCss.menuSpace} ${menuClass} ${onClick && 'cursor-pointer'}`}
+    className={`${MenulistCss.menuItem} ${
+      !link && MenulistCss.menuSpace
+    } ${menuClass} ${onClick && 'cursor-pointer'}`}
     onClick={onClick && onClick}
   >
     {!!icon && (
       <div className={`w-4 h-4 ${iconClass}`}>
         {icon}
         {!!tooltip && (
-        <ReactTooltip id={`tooltip${tooltip}`} type="dark">
-          {tooltip}
-        </ReactTooltip>
+          <ReactTooltip id={`tooltip${tooltip}`} type="dark">
+            {tooltip}
+          </ReactTooltip>
         )}
       </div>
     )}
     {link ? (
-      <Link
-        to={link}
-        className={MenulistCss.menuListLink}
-
-      >
+      <Link to={link} className={MenulistCss.menuListLink}>
         {!!title && (
-        <span className={`text-primary-text text-sm whitespace-nowrap overflow-hidden overflow-ellipsis block ${icon && 'ml-2'} ${textCss}`}>
-          {title}
-        </span>
-        )}
-
-      </Link>
-    )
-      : (
-        <div>
-          {!!title && (
-          <span className={`text-primary-text text-sm whitespace-nowrap overflow-hidden overflow-ellipsis block ${icon && 'ml-2'} ${textCss}`}>
+          <span
+            className={`text-primary-text text-sm whitespace-nowrap overflow-hidden overflow-ellipsis block ${
+              icon && 'ml-2'
+            } ${textCss}`}
+          >
             {title}
           </span>
-          )}
-
-        </div>
-      )}
-
+        )}
+      </Link>
+    ) : (
+      <div>
+        {!!title && (
+          <span
+            className={`text-primary-text text-sm whitespace-nowrap overflow-hidden overflow-ellipsis block ${
+              icon && 'ml-2'
+            } ${textCss}`}
+          >
+            {title}
+          </span>
+        )}
+      </div>
+    )}
   </div>
 );
+
+MenuBox.defaultProps = {
+  tooltip: false,
+};
--- packages/client/src/components/MenuList/menuListCss.js
+++ packages/client/src/components/MenuList/menuListCss.js
@@ -1,5 +1,5 @@
 export const MenulistCss = {
   menuItem: 'flex items-center hover:bg-gray-100 ',
-  menuListLink: 'pt-2 pb-2 pl-3 pr-3',
-  menuSpace: 'pt-2 pb-2 pl-3 pr-3',
+  menuListLink: 'py-2 px-3',
+  menuSpace: 'py-2 px-3',
 };
